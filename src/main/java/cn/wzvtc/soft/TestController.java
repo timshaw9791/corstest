87650d427cc74149f848a7f40f1c3e59b94f02e8
timshaw9791 <84539606@qq.com>
2020-04-17 07:54:34 +0800

    提交修改

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -8,16 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-//@CrossOrigin
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController("/" )
 public class TestController {
 
     
     @RequestMapping(value="/data.json",method={RequestMethod.GET})
     public Map<String,String> bookById(){
-        Map<String,String> resultMap=new HashMap<>();
-        resultMap.put("myname","刘笑锋1");
-        resultMap.put("mynumber","2012020045");
-        return resultMap;
-    }
+    Map<String,String> resultMap=new HashMap<>();
+    resultMap.put("myname","刘笑锋1");
+    resultMap.put("mynumber","2012020045");
+    return resultMap;
+}
 }
