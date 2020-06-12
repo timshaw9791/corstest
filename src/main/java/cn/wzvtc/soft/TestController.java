86eaa4011cb7bde1215d13a94f9736748441db0d
timliu <84539606@qq.com>
2020-06-12 09:30:10 +0800

    lesson17-login/logout

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -1,5 +1,6 @@
 package cn.wzvtc.soft;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,17 +12,43 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "http://127.0.0.1:5500",allowCredentials = "true")
 @RestController("/")
 public class TestController {
 
     @RequestMapping(value = "/data.json")
-    public List bookById(HttpSession httpSession) {
+    public List bookByIdWithAuthened(HttpSession httpSession) {
+        String username = (String) httpSession.getAttribute("username");
+        if (StringUtils.hasText(username)) {
+            return bookById();
+        }else{
+            return null;
+        }
+    }
 
-        System.out.println(httpSession.getId());
-        httpSession.invalidate();
-        httpSession.
+    @RequestMapping(value = "/login")
+    public boolean login(String username,String password,HttpSession httpSession) {
+        if("2012020045".equals(username) && "lxf".equals(password)){
+            httpSession.setAttribute("useranme","2012020045");
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @RequestMapping(value = "/logout")
+    public void logout(HttpSession httpSession) {
+        httpSession.removeAttribute("username");
+    }
+
+
+
+
+
+
+
+    private List bookById() {
+
         List resultList=new ArrayList();
         Map resultMap = new HashMap<>();
         resultMap.put("name", "计算机安装与维护");
