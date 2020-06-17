64a71fc1d5aedf803b8abcb9819ab5e71463f68f
timliu <84539606@qq.com>
2020-06-17 13:19:22 +0800

    完成初步的登陆注销功能代码

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -12,16 +12,12 @@ import java.util.List;
 import java.util.Map;
 
 
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "http://127.0.0.1:5500",allowCredentials = "true")
 @RestController("/")
 public class TestController {
 
     @RequestMapping(value = "/data.json")
-    public List bookById(HttpSession httpSession) {
-
-        System.out.println(httpSession.getId());
-        httpSession.invalidate();
-        httpSession.
+    public List bookById() {
         List resultList=new ArrayList();
         Map resultMap = new HashMap<>();
         resultMap.put("name", "计算机安装与维护");
@@ -49,11 +45,30 @@ public class TestController {
 
 
     @RequestMapping(value = "/creditByName")
-    public int getCrditByName(String name) {
+    public int getCrditByName(String name,HttpSession httpSession) {
+        if(httpSession.getAttribute("username")==null){
+            return -1;
+        }
+
         if("计算机编程基础".equals(name)){
             return 4;
         }else {
             return 2;
         }
+    }
+
+    @RequestMapping(value="/login")
+    public boolean login(String password,String username,HttpSession httpSession){
+        if("lxf".equals(password) && "2012020045".equals(username)){
+            httpSession.setAttribute("username","username");
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @RequestMapping(value="/logout")
+    public void llogoutogin(HttpSession httpSession){
+        httpSession.invalidate();
     }
 }
